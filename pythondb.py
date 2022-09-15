5e6a023322e6d750c2c0cb9cb7cd201e6fbefdd0
08Shashankb <shashank862b@gmail.com>
2022-09-15 15:35:16 +0530

    "changed pythondb"

--- pythondb.py
+++ pythondb.py
@@ -2,7 +2,7 @@ import mariadb
 import sys
 conn = mariadb.connect(
     user="root",
-    password="incorrect@el1",
+    password="incorrect",
     host="localhost",
     port=3306,
     database="employee"
